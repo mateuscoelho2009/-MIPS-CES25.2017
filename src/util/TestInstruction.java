7eec894a2420b05dc75eb05d9fed40aa08af52b6
thayza.tbz@gmail.com <thayza.tbz@gmail.com>
2017-06-20 15:18:54 -0300

    Instruction Class completed with simple test

--- src/util/TestInstruction.java
+++ src/util/TestInstruction.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import util.Instruction.INSTR_TYPE;
+
 public class TestInstruction {
 
 	@Test
@@ -11,12 +13,16 @@ public class TestInstruction {
 		Instruction add = new Instruction("00000000001001110100100000100000");
 		Instruction addi = new Instruction("00100000000000010000000000000011");
 		
-		assertEquals(add.getRS(), 9);
-		assertEquals(add.getRT(), 1);
-		assertEquals(add.getRD(), 7);
+		assertEquals(add.getType(), INSTR_TYPE.R);
+		assertEquals(add.getMnemonic(), Instruction.ADD);
+		assertEquals(add.getRS(), 1);
+		assertEquals(add.getRT(), 7);
+		assertEquals(add.getRD(), 9);
 		
-		assertEquals(addi.getRS(), 1);
-		assertEquals(addi.getRT(), 0);
+		assertEquals(addi.getType(), INSTR_TYPE.I);
+		assertEquals(addi.getMnemonic(), Instruction.ADDI);
+		assertEquals(addi.getRS(), 0);
+		assertEquals(addi.getRT(), 1);
 		assertEquals(addi.getImmediate(), 3);
 	}
 
