9dc5fe7065305d0abf7c7096cb750e1fa95b7c7d
thayza.tbz@gmail.com <thayza.tbz@gmail.com>
2017-06-20 14:37:49 -0300

    Get rs, rt, rd

--- src/util/Instruction.java
+++ src/util/Instruction.java
@@ -18,13 +18,18 @@ public class Instruction {
 	
 	String instr_mnemonic_;
 	INSTR_TYPE type_;
+	int rs, rt, rd;
 	
 	public Instruction (String byteCode) {
 		String opcode = byteCode.substring(0, 6);
 		switch(opcode) {
 			case TYPER: type_ = INSTR_TYPE.R; break;
 			case ADDI: case BEQ: case BLE: case LW: case SW:
-				//type_ = INS
+				type_ = INSTR_TYPE.I; instr_mnemonic_ = opcode;
+				break;
+			case JMP: type_ = INSTR_TYPE.J; instr_mnemonic_ = opcode;
+				break;
+			default: type_ = INSTR_TYPE.UNDEFINED; instr_mnemonic_ = opcode;
 		}
 	}
 	
@@ -34,5 +39,17 @@ public class Instruction {
 	
 	INSTR_TYPE getType () {
 		return type_;
+	}
+	
+	int getRS() {
+		return rs;
+	}
+	
+	int getRT() {
+		return rt;
+	}
+	
+	int getRD() {
+		return rd;
 	}
 }
