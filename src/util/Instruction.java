077ff72bd2cf46cfb4f8de4843fcc2c7c236b55d
thayza.tbz@gmail.com <thayza.tbz@gmail.com>
2017-06-20 14:21:38 -0300

    Editing Instruction Class

--- src/util/Instruction.java
+++ src/util/Instruction.java
@@ -1,5 +1,21 @@
 package util;
 
 public class Instruction {
-	static public String {ADD = 000000};
+	static public String TYPER = "000000",
+						ADD = "100000", 
+						ADDI = "001000",
+						BEQ = "000101",
+						BLE = "000111",
+						BNE = "000100",
+						JMP = "000010",
+						LW = "100011",
+						MUL = "011000",
+						NOP = "000000",
+						SUB = "100010",
+						SW = "101011";
+						// O que é o LI??
+	static public enum INTR_TYPE {R, I, J, UNDEFINED};
+	
+	String intr_mnemonic_;
+	INTR_TYPE type_;
 }
