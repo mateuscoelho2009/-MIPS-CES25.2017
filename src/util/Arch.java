06e0a347610c9987e150bedc0473bccc7675a9ee
Mateus Menezes Azevedo Coelho <mateuscoelho2009@gmail.com>
2017-06-20 15:57:25 -0300

    Arch with vector of instructions

--- src/util/Arch.java
+++ src/util/Arch.java
@@ -1,9 +1,26 @@
 package util;
 
+import java.util.Vector;
+
 public class Arch {
+	Vector<Instruction> program;
+	int pc_ = 0;
+	boolean terminated = false;
+	
 	public static Register r = new Register();
 	public static Memory m = new Memory(4000);
 	public static ULA ula = new ULA();
+	public Instruction getNextInstruction () {
+		if (pc_ < 0 || pc_ > program.size()) {
+			pc_ = 0;
+			terminated = true;
+		}
+		
+		Instruction next = program.get(pc_);
+		pc_++;
+		
+		return next;
+	}
 	
     public static void main(String[] args) {}
 }
--- src/utils/Instruction.java
+++
@@ -1,8 +0,0 @@
-package utils;
-
-public class Instruction {
-	
-	Instruction (String byteCode) {
-		
-	}
-}
