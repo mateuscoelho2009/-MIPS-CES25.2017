85c65fe054725e27268d6a142a3d74bdb872df41
mknarciso <murilokn@gmail.com>
2017-06-20 15:22:40 -0300

    ula2

--- src/util/ULA.java
+++ src/util/ULA.java
@@ -2,41 +2,43 @@ package util;
 
 public class ULA {
 	private int rs,rt,rd,target,immediate,ticker;
-	private String shamt, funct;
+	private String mnemonic;
 	public ULA(){
 		// Empty
 	}
-	public String set(Instruction inst){
+	public void set(Instruction inst){
 		switch (inst.getType()) {
-			case R:	artimethic(inst.getRs(), inst.getRt(), inst.getRd() inst.getShamt(), inst.getFunct());
+			case R:	arithmetic(inst.getRS(), inst.getRT(), inst.getRD(), inst.getMnemonic());
 					break;
-			case I: immediate(inst.getMinemonico(), inst.getRS(), inst.getRt(), inst.getImmediate());
+			case I: immediate(inst.getRS(), inst.getRT(), inst.getImmediate(), inst.getMnemonic());
 					break;
-			case J: jump(inst.getTarget());
+			case J: jump(inst.getTargetAddress(), inst.getMnemonic());
 					break;
 			default: System.out.println("Instrução Incorreta");
 					break;
 		}
 	}
-	private void arithmetic(int rs, int rt, int rd, String shamt, String funct){
+	private void jump(int targetAddress, String mnemonic) {
+		this.target = targetAddress;
+		this.mnemonic = mnemonic;
+		ticker = 1;
+		
+	}
+	private void arithmetic(int rs, int rt, int rd, String mnemonic){
 			this.rs = rs; this.rt = rt; this.rd = rd;
-			this.shamt = shamt; this.funct = funct;
-			if(!srtcmp(funct,MUL))
+			this.mnemonic = mnemonic;
+			if(mnemonic.equals(Instruction.MUL))
 				ticker = 3;
 			else
 				ticker = 1;
 	}	
-	private void immediate(String minemonico, int rs, int rt, int immediate){
+	private void immediate(int rs, int rt, int immediate, String mnemonic){
 		this.rs = rs; this.rt = rt; this.immediate = immediate;
-		if(!srtcmp(minemonico,LW)||!srtcmp(minemonico,SW))
+		this.mnemonic = mnemonic;
+		if(mnemonic.equals(Instruction.LW)||!mnemonic.equals(Instruction.SW))
 			ticker = 4;
 		else
 			ticker = 1;
 	}
-	private void tick(){
-		switch (funct) {
-		case ADD:	Arch.r.write(rd,Arch.r.read(rt)+Arch.r.read(rs));
-					break;
-		}
-	}
+
 }
